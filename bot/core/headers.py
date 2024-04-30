26c977fdd0fac2c2a93819b5fd1872318cf66e88
shamhi <shomakakdela@gmail.com>
2024-04-30 13:19:25 +0300

    📌 Initial commit

---
+++ bot/core/headers.py
@@ -0,0 +1,14 @@
+headers = {
+    'accept': 'application/json, text/plain, */*',
+    'accept-language': 'ru,ru-RU;q=0.9,en-US;q=0.8,en;q=0.7',
+    'origin': 'https://www.yescoin.gold',
+    'referer': 'https://www.yescoin.gold/',
+    'sec-ch-ua': '"Android WebView";v="123", "Not:A-Brand";v="8", "Chromium";v="123"',
+    'sec-ch-ua-mobile': '?1',
+    'sec-ch-ua-platform': '"Android"',
+    'sec-fetch-dest': 'empty',
+    'sec-fetch-mode': 'cors',
+    'sec-fetch-site': 'same-site',
+    'user-agent': 'Mozilla/5.0 (Linux; Android 13; RMX3630 Build/TP1A.220905.001; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/123.0.6312.118 Mobile Safari/537.36',
+    'x-requested-with': 'org.telegram.messenger',
+}
